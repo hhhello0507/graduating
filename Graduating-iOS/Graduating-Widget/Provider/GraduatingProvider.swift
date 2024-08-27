55f52c998dfa3f57c2746461874712cab31f39d9
hhhello <hhhello0507@dgsw.hs.kr>
2024-08-27 19:20:19 +0900

    위젯 수정~ ㅎ하ㅏ하하하

--- Graduating-iOS/Graduating-Widget/Provider/GraduatingProvider.swift
+++ Graduating-iOS/Graduating-Widget/Provider/GraduatingProvider.swift
@@ -11,30 +11,30 @@ import Data
 import Model
 
 struct GraduatingProvider: TimelineProvider {
-    func placeholder(in context: Context) -> GraduatingEntry {
-        GraduatingEntry(
-            date: .now,
-            remainTime: .init(),
-            remainTimePercent: 40
-        )
+    
+    typealias Entry = GraduatingEntry
+    
+    func placeholder(in context: Context) -> Entry {
+        .empty
     }
 
-    func getSnapshot(in context: Context, completion: @escaping (Entry) -> ()) {
-//        let entry = GraduatingEntry(date: Date(), emoji: "😀")
-//        completion(entry)
+    func getSnapshot(in context: Context, completion: @escaping (Entry) -> Void) {
+        completion(.empty)
     }
 
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        let currentTime = Date()
-        let entryDate = Calendar.current.date(byAdding: .hour, value: 1, to: currentTime)!
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
+        let currentTime = Date.now
+        let afterDate = Calendar.current.date(byAdding: .hour, value: 1, to: currentTime)!
         
         guard let grade = UserDefaultsType.grade.value as? Int,
               let graduatingJson = UserDefaultsType.graduating.value as? String,
               let graduating = Graduating.decode(graduatingJson) else {
+            print("invalid grade")
             return
         }
         guard let schoolJson = UserDefaultsType.school.value as? String,
               let school = School.decode(schoolJson) else {
+            print("invalid school")
             return
         }
         
@@ -42,6 +42,7 @@ struct GraduatingProvider: TimelineProvider {
         
         guard let startAt = Date.getStartAt(for: grade),
               let adjustedEndAt = graduating.graduatingDay.adjustedEndAt(for: grade, limit: limit) else {
+            print("invalid time")
             return
         }
         
@@ -49,11 +50,11 @@ struct GraduatingProvider: TimelineProvider {
         let remainTimePercent = currentTime.percent(from: startAt, to: adjustedEndAt)
         
         let entry = GraduatingEntry(
-            date: entryDate,
+            date: currentTime,
             remainTime: remainTime,
             remainTimePercent: remainTimePercent
         )
-        let timeline = Timeline(entries: [entry], policy: .after(entryDate))
+        let timeline = Timeline(entries: [entry], policy: .after(afterDate))
         completion(timeline)
     }
 }
