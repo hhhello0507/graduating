c2f62c9f4c57eeeec45eef0db625d538d50d7160
hhhello <hhhello0507@dgsw.hs.kr>
2024-08-28 12:34:09 +0900

    SchoolType

--- Graduating-iOS/Model/School/School.swift
+++ Graduating-iOS/Model/School/School.swift
@@ -27,17 +27,3 @@ public struct School: ModelProtocol {
         self.anniversary = anniversary
     }
 }
-
-public enum SchoolType: String, ModelProtocol {
-    case HIGH
-    case MIDDLE
-    case ELEMENTARY
-    
-    public var limit: Int {
-        switch self {
-        case .HIGH: 3
-        case .MIDDLE: 3
-        case .ELEMENTARY: 6
-        }
-    }
-}
