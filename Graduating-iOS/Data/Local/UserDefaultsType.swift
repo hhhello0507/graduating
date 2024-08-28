96860a95f11d9725c5c67b81ffbf5abff0aa0ecd
hhhello <hhhello0507@dgsw.hs.kr>
2024-08-28 11:26:14 +0900

    적용 MyMoya 타겟

--- Graduating-iOS/Data/Local/UserDefaultsType.swift
+++ Graduating-iOS/Data/Local/UserDefaultsType.swift
@@ -6,23 +6,7 @@
 //
 
 import Foundation
-
-public protocol UserDefaultsProtocol: RawRepresentable<Self.Key> {
-    
-    typealias Key = String
-
-    var userDefaults: UserDefaults { get }
-}
-
-public extension UserDefaultsProtocol {
-    var value: Any? {
-        userDefaults.value(forKey: self.rawValue)
-    }
-    
-    func set(_ value: Any?) {
-        userDefaults.setValue(value, forKey: self.rawValue)
-    }
-}
+import MyShared
 
 public enum UserDefaultsType: String, UserDefaultsProtocol {
     case school
