2ccbbfc839dfb073d6cc37f3448433d3d68648e3
hhhello <hhhello0507@dgsw.hs.kr>
2024-10-04 11:59:59 +0900

    Fix server

--- Graduating-Server/src/main/kotlin/com/bestswlkh0310/graduating/graduatingserver/core/school/SchoolRepository.kt
+++ Graduating-Server/src/main/kotlin/com/bestswlkh0310/graduating/graduatingserver/core/school/SchoolRepository.kt
@@ -1,11 +1,13 @@
 package com.bestswlkh0310.graduating.graduatingserver.core.school
 
+import com.bestswlkh0310.graduating.graduatingserver.global.exception.CustomException
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Repository
 
 @Repository
 interface SchoolRepository : JpaRepository<SchoolEntity, Long>
 
 fun SchoolRepository.getBy(id: Long) =
-    findByIdOrNull(id) ?: throw Exception("No school with id $id")
+    findByIdOrNull(id) ?: throw CustomException(HttpStatus.NOT_FOUND, "Not found school")
--- Graduating-Server/src/main/kotlin/com/bestswlkh0310/graduating/graduatingserver/core/user/User.kt
+++ Graduating-Server/src/main/kotlin/com/bestswlkh0310/graduating/graduatingserver/core/user/User.kt
@@ -20,5 +20,5 @@ class User(
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    val platformType: PlatformType = PlatformType.DEFAULT
+    val platformType: PlatformType
 )
