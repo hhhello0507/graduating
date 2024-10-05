dd90f4888885fe433a5f384cd6449176b60eeeb5
hhhello <hhhello0507@dgsw.hs.kr>
2024-10-05 23:06:16 +0900

    Fix MealRepository

--- Graduating-Server/src/main/kotlin/com/bestswlkh0310/graduating/graduatingserver/core/meal/MealRepository.kt
+++ Graduating-Server/src/main/kotlin/com/bestswlkh0310/graduating/graduatingserver/core/meal/MealRepository.kt
@@ -4,12 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDate
 
 @Repository
 interface MealRepository: JpaRepository<MealEntity, Long> {
     fun findBySchoolIdAndMealDate(schoolId: Long, mealDate: LocalDate): List<MealEntity>
 
+    @Transactional
     @Modifying
     @Query("DELETE FROM MealEntity e WHERE e.mealDate < CURRENT_DATE")
     fun deleteOldRecords()
