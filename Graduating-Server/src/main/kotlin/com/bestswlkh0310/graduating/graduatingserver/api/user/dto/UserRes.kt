6d88df37634bea38eca472e9a905e72d1cc7cc7e
hhhello <hhhello0507@dgsw.hs.kr>
2024-10-06 21:10:39 +0900

    Fix UserRes

--- Graduating-Server/src/main/kotlin/com/bestswlkh0310/graduating/graduatingserver/api/user/dto/UserRes.kt
+++ Graduating-Server/src/main/kotlin/com/bestswlkh0310/graduating/graduatingserver/api/user/dto/UserRes.kt
@@ -2,21 +2,23 @@ package com.bestswlkh0310.graduating.graduatingserver.api.user.dto
 
 import com.bestswlkh0310.graduating.graduatingserver.core.school.SchoolEntity
 import com.bestswlkh0310.graduating.graduatingserver.core.user.UserEntity
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class UserRes(
     val id: Long,
     val email: String,
-    val nickname: String,
-    val graduatingYear: Int,
-    val school: SchoolEntity
+    val nickname: String?,
+    val graduatingYear: Int?,
+    val school: SchoolEntity?
 ) {
     companion object {
         fun of(user: UserEntity) = UserRes(
             id = user.id, 
             email = user.email,
-            nickname = user.nickname!!,
-            graduatingYear = user.graduatingYear!!,
-            school = user.school!!
+            nickname = user.nickname,
+            graduatingYear = user.graduatingYear,
+            school = user.school
         )
     }
 }
