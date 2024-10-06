5349f0aa3c5231f0cc516a2e759c644e7f8e2cfe
hhhello <hhhello0507@dgsw.hs.kr>
2024-10-06 18:35:50 +0900

    Apply server changes

--- Graduating-iOS/Model/User/User.swift
+++ Graduating-iOS/Model/User/User.swift
@@ -2,20 +2,20 @@ import Foundation
 
 public struct User: ModelProtocol {
     public let id: Int
-    public let username: String
+    public let email: String
     public let nickname: String
     public let graduatingYear: Int
     public let school: School
     
     public init(
         id: Int,
-        username: String,
+        email: String,
         nickname: String,
         graduatingYear: Int,
         school: School
     ) {
         self.id = id
-        self.username = username
+        self.email = email
         self.nickname = nickname
         self.graduatingYear = graduatingYear
         self.school = school
