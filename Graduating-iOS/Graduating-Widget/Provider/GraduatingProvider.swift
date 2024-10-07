1429c786b9da40cb68175850103ddfae41c46f70
hhhello <hhhello0507@dgsw.hs.kr>
2024-10-07 16:20:06 +0900

    Fix widget

--- Graduating-iOS/Graduating-Widget/Provider/GraduatingProvider.swift
+++ Graduating-iOS/Graduating-Widget/Provider/GraduatingProvider.swift
@@ -1,16 +1,10 @@
-//
-//  GraduatingProvider.swift
-//  Graduating-WidgetExtension
-//
-//  Created by hhhello0507 on 8/24/24.
-//
-
+import Combine
 import WidgetKit
 import Shared
 import Data
-import MyFoundationUtil
 
-struct GraduatingProvider: TimelineProvider {
+class GraduatingProvider: TimelineProvider {
+    private var subscriptions = Set<AnyCancellable>()
     
     typealias Entry = GraduatingEntry
     
@@ -26,36 +20,20 @@ struct GraduatingProvider: TimelineProvider {
         let currentTime = Date.now
         let afterDate = Calendar.current.date(byAdding: .hour, value: 1, to: currentTime)!
         
-        // TODO: Fix
-//        guard let grade = UserDefaultsType.grade.value as? Int,
-//              let graduatingJson = UserDefaultsType.graduating.value as? String,
-//              let graduating = Graduating.decode(graduatingJson) else {
-//            print("invalid grade")
-//            return
-//        }
-//        guard let schoolJson = UserDefaultsType.school.value as? String,
-//              let school = School.decode(schoolJson) else {
-//            print("invalid school")
-//            return
-//        }
-//        
-//        let limit = school.type?.limit ?? 3
-//        
-//        guard let startAt = Date.getStartAt(for: grade),
-//              let adjustedEndAt = graduating.graduatingDay.adjustedEndAt(for: grade, limit: limit) else {
-//            print("invalid time")
-//            return
-//        }
-//        
-//        let remainTime: DateComponents = currentTime.diff([.year, .month, .day, .hour, .minute, .second, .nanosecond], other: adjustedEndAt)
-//        let remainTimePercent = currentTime.percent(from: startAt, to: adjustedEndAt)
-//        
-//        let entry = GraduatingEntry(
-//            date: currentTime,
-//            remainTime: remainTime,
-//            remainTimePercent: remainTimePercent
-//        )
-//        let timeline = Timeline(entries: [entry], policy: .after(afterDate))
-//        completion(timeline)
+        UserService.shared.getMe()
+            .ignoreError()
+            .sink { user in
+                guard let graduating = user.graduating,
+                      let remainTime = graduating.remainTime,
+                      let remainTimePercent = graduating.remainTimePercent else { return }
+                let entry = GraduatingEntry(
+                    date: currentTime,
+                    remainTime: remainTime,
+                    remainTimePercent: remainTimePercent
+                )
+                let timeline = Timeline(entries: [entry], policy: .after(afterDate))
+                completion(timeline)
+            }
+            .store(in: &self.subscriptions)
     }
 }
