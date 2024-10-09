3a62de525d4c296438cc216e99794b861f12d96c
hhhello <hhhello0507@dgsw.hs.kr>
2024-10-09 20:24:47 +0900

    Apply MyIOSKit Changes

--- Graduating-iOS/Graduating/Feature/Main/MainView.swift
+++ Graduating-iOS/Graduating/Feature/Main/MainView.swift
@@ -2,53 +2,25 @@ import SwiftUI
 import Shared
 import MyDesignSystem
 
-enum Item: BottomAppBarItem {
-    case home
-    case meal
-    case profile
-    
-    var icon: Iconable {
-        switch self {
-        case .home: Icons.Feature.Home
-        case .meal: Icons.Feature.Utensils
-        case .profile: Icons.Feature.Person
-        }
-    }
-    var text: String {
-        switch self {
-        case .home: "홈"
-        case .meal: "급식"
-        case .profile: "프로필"
-        }
-    }
-}
-
-let data = [
-    Item.home,
-    Item.meal,
-    Item.profile
-]
-
 struct MainView {
     @EnvironmentObject private var router: Router
     @EnvironmentObject private var appState: AppState
     @EnvironmentObject private var dialogProvider: DialogProvider
     
-    @State private var selectedTab = data[0]
+    @State private var selection = 0
     
     init() {}
 }
 
 extension MainView: View {
     var body: some View {
-        MyBottomAppBar(data, selection: selectedTab) {
-            selectedTab = $0
-        } content: {
-            switch selectedTab {
-            case .home: HomeView()
-            case .meal: MealView()
-            case .profile: ProfileView()
-            }
+        MyBottomAppBar(selection: $selection) {
+            HomeView()
+                .page(icon: Icons.Feature.Home, text: "홈")
+            MealView()
+                .page(icon: Icons.Feature.Utensils, text: "급식")
+            ProfileView()
+                .page(icon: Icons.Feature.Person, text: "프로필")
         }
     }
 }
