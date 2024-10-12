bf281a0ec382e2347f04f96470484caf30bedf4d
hhhello <hhhello0507@dgsw.hs.kr>
2024-10-12 19:58:13 +0900

    Fix widget custom palette

--- Graduating-iOS/Graduating-Widget/Graduating_WidgetBundle.swift
+++ Graduating-iOS/Graduating-Widget/Graduating_WidgetBundle.swift
@@ -1,8 +1,16 @@
 import WidgetKit
 import SwiftUI
+import MyDesignSystem
 
 @main
 struct Graduating_WidgetBundle: WidgetBundle {
+    init() {
+        if let theme = UserDefaults.graduating.value(forKey: "theme") as? Int,
+           let pallete = Palette(rawValue: theme) {
+            CustomPalette.Provider()
+                .updateColor(pallete: pallete)
+        }
+    }
     var body: some Widget {
         GraduatingWidget()
         MealWidget()
